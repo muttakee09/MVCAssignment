ebe06cee985f00be32b8257350af54b676c3545e
ENOSISBD\muttakee <saad.muttakee@enosisbd.com>
2021-08-24 13:42:32 +0600

    - added structuremap. - added dependency injection in student controller.

--- MVCAssignment/Controllers/StudentController.cs
+++ MVCAssignment/Controllers/StudentController.cs
@@ -7,57 +7,45 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MVCAssignment.Services;
 
 namespace MVCAssignment.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
         // GET: Student
         public ActionResult Index()
         {
-            using (ISession session = NHibernateSessions.OpenSession())
-            {
-                var students = session.Query<Student>().ToList();
-                return View(students);
-            }
+            return View(_studentService.GetStudentList());
         }
 
         public ActionResult Details(int? id)
         {
-            using (ISession session = NHibernateSessions.OpenSession())
+            if (id != null)
             {
-                ViewBag.Message = "Student ID: " + id;
-                var student = session.Get<Student>(id);
-                if (student.MainCourse != null)
-                {
-                    student.MainCourse = session.Get<Course>(student.MainCourse.CourseId);
-
-                }
-                if (student.SupplementaryCourse != null)
-                {
-                    student.SupplementaryCourse = session.Get<Course>(student.SupplementaryCourse.CourseId);
-
-                }
-                return View(student);
+                Student s = _studentService.GetStudentWithCourseDetails((int)id);
+                return View(s);
             }
+            else
+            {
+                return HttpNotFound();
+            }
+
         }
 
         public ActionResult Create()
         {
-            using (ISession session = NHibernateSessions.OpenSession())
-            {
-                var courseList = session.Query<Course>()
-                    .ToList();
-                ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
-                /*var courseList = session.Query<Course>()
-                    .Select((r) => new SelectListItem
-                    { Text = r.CourseCode + " - " + r.CourseName, Value = r.CourseId.ToString()
-                    })
-                    .ToList();
-                ViewBag.CourseList = courseList;*/
-                return View();
-            }
-            
+            IList<Course> courseList = _studentService.GetCourseList();
+            ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
+            return View();
+
         }
 
         [HttpPost]
@@ -101,37 +89,23 @@ namespace MVCAssignment.Controllers
             }
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
             if (id != null)
             {
-                using (ISession session = NHibernateSessions.OpenSession())
-                {
-                    ViewBag.Message = "Student ID: " + id;
-                    var student = session.Get<Student>(id);
-                    var courseList = session.Query<Course>()
-                     .ToList();
-                    if (student.MainCourse != null)
-                    {
-                        student.MainCourseId = student.MainCourse.CourseId;
-
-                    }
-                    if (student.SupplementaryCourse != null)
-                    {
-                        student.SupplementaryCourseId = student.SupplementaryCourse.CourseId;
-
-                    }
-                    ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
-                    return View(student);
-                }
+                Student student = _studentService.GetStudent((int)id);
+                IList<Course> courseList = _studentService.GetCourseList();
+                ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
+                return View(student);
             }
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
 
-        [HttpPost, ActionName("Edit")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(
-            Include = "StudentName, Age, Image, BloodGroup, Gender, MainCourse, SupplementaryCourse")] int? id, Student student, HttpPostedFileBase image)
+            Include = "StudentName, Age, Image, BloodGroup, Gender, MainCourse, SupplementaryCourse")] int? id,
+            Student student, HttpPostedFileBase image)
         {
             if (id == null)
             {
@@ -140,40 +114,43 @@ namespace MVCAssignment.Controllers
             
             using (ISession session = NHibernateSessions.OpenSession())
             {
-                try
+                using (ITransaction transaction = session.BeginTransaction())
                 {
-                    var alreadySavedStudent = session.Get<Student>(id);
-                    var courseList = session.Query<Course>()
-                        .ToList();
-                    ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
-
-                    if (image != null && image.ContentLength > 0)
+                    try
                     {
-                        string fileName = Path.GetFileName(image.FileName);
-                        string path = Path.Combine(Server.MapPath("~/Photos"), fileName);
-                        image.SaveAs(path);
-                        student.Image = Path.GetFileName(image.FileName);
+                        var alreadySavedStudent = session.Get<Student>(id);
+                        var courseList = session.Query<Course>()
+                            .ToList();
+                        ViewBag.CourseList = new SelectList(courseList, "CourseId", "CourseName");
 
+                        if (image != null && image.ContentLength > 0)
+                        {
+                            string fileName = Path.GetFileName(image.FileName);
+                            string path = Path.Combine(Server.MapPath("~/Photos"), fileName);
+                            image.SaveAs(path);
+                            student.Image = Path.GetFileName(image.FileName);
+
+                        }
+                        else
+                        {
+                            student.Image = alreadySavedStudent.Image;
+                        }
+                        session.Evict(alreadySavedStudent);
+
+                        var MCourse = courseList.FirstOrDefault(s => s.CourseId == student.MainCourseId);
+                        var SCourse = courseList.FirstOrDefault(s => s.CourseId == student.SupplementaryCourseId);
+
+                        student.MainCourse = MCourse;
+                        student.SupplementaryCourse = SCourse;
+                        session.Update(student);
+                        transaction.Commit();
+                        return RedirectToAction("Index");
                     }
-                    else
+                    catch (Exception e)
                     {
-                        student.Image = alreadySavedStudent.Image;
+                        ViewBag.Message = e.Message;
+                        return View();
                     }
-                    session.Evict(alreadySavedStudent);
-
-                    var MCourse = courseList.First(s => s.CourseId == student.MainCourseId);
-                    var SCourse = courseList.First(s => s.CourseId == student.SupplementaryCourseId);
-                    
-                    student.MainCourse = MCourse;
-                    student.SupplementaryCourse = SCourse;
-                    session.Evict(student);
-                    session.Update(student);
-                    return RedirectToAction("Index");
-                }
-                catch (Exception e)
-                {
-                    ViewBag.Message = e.Message;
-                    return View();
                 }
             }
         }
@@ -188,31 +165,15 @@ namespace MVCAssignment.Controllers
             {
                 ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
             }
-            using (ISession session = NHibernateSessions.OpenSession())
-            {
-                var student = session.Get<Student>(id);
-                if (student == null)
-                {
-                    return HttpNotFound();
-                }
-                return View(student);
-            }
-            
+            Student s = _studentService.GetStudent((int)id);
+            return View(s);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, Student student)
         {
-            using (ISession session = NHibernateSessions.OpenSession())
-            {
-                using (ITransaction transaction = session.BeginTransaction())
-                {
-                    var std = session.Get<Student>(id);
-                    session.Delete(std);
-                    transaction.Commit();
-                }
-            }
+            _studentService.DeleteStudent(id);
             return RedirectToAction("Index");
         }
     }
--- MVCAssignment/Services/StudentServices.cs
+++ MVCAssignment/Services/StudentServices.cs
@@ -2,11 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MVCAssignment.Models;
+using NHibernate;
 
 namespace MVCAssignment.Services
 {
-    public class StudentServices
+    public interface IStudentService
+    {
+        Student GetStudent(int id);
+        Course GetCourse(int id);
+        Student GetStudentWithCourseDetails(int id);
+        IList<Student> GetStudentList();
+        IList<Course> GetCourseList();
+        void CreateStudent(Student std);
+        void UpdateStudent(Student std);
+        void DeleteStudent(int id);
+    }
+    public class StudentServices : IStudentService
     {
 
+        public Student GetStudent(int id)
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                var student = session.Get<Student>(id);
+                return student;
+            }
+        }
+
+        public Course GetCourse(int id)
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                var course = session.Get<Course>(id);
+                return course;
+            }
+        }
+
+        public Student GetStudentWithCourseDetails(int id)
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                var student = session.Get<Student>(id);
+                if (student.MainCourse != null)
+                {
+                    student.MainCourse = session.Get<Course>(student.MainCourse.CourseId);
+
+                }
+                if (student.SupplementaryCourse != null)
+                {
+                    student.SupplementaryCourse = session.Get<Course>(student.SupplementaryCourse.CourseId);
+
+                }
+                return student;
+            }
+        }
+
+        public IList<Student> GetStudentList()
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                var students = session.Query<Student>().ToList();
+                return students;
+            }
+        }
+
+        public IList<Course> GetCourseList()
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                var courses = session.Query<Course>().ToList();
+                return courses;
+            }
+        }
+
+        public void CreateStudent(Student student)
+        {
+
+        }
+
+        public void UpdateStudent(Student student)
+
+        {
+
+        }
+
+        public void DeleteStudent(int id)
+        {
+            using (ISession session = NHibernateSessions.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    var std = session.Get<Student>(id);
+                    session.Delete(std);
+                    transaction.Commit();
+                }
+            }
+        }
     }
+
 }
